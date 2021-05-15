b966a28ddc7a125747d35323d6118a6a91961f40
Francisco <javiermartincuenca@gmail.com>
2021-05-15 19:33:07 +0200

    Testint App component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,15 +1,37 @@
-import { shallow, ShallowWrapper } from 'enzyme';
+import { mount, ReactWrapper } from 'enzyme';
 import { findByTestAttr } from './test/testUtils';
 import App from './App';
 
+// activate global mock to make sure getSecretWord does not make network call
+jest.mock('./actions');
+// eslint-disable-next-line import/first
+import { getSecretWord as mockGetSecretWord } from './actions';
 /**
  * Setup function for App component
- * @returns {ShallowWrapper}
+ * @returns {ReactWrapper}
  */
-const setup = (): ShallowWrapper => shallow(<App />);
+const setup = (): ReactWrapper => mount(<App />);
 
 test('renders without error', () => {
   const wrapper = setup();
   const appComponent = findByTestAttr(wrapper, 'component-app');
   expect(appComponent).toHaveLength(1);
+});
+
+describe('get secret word', () => {
+  beforeEach(() => {
+    mockGetSecretWord.mockClear();
+  });
+
+  test('get secret word on app mount', () => {
+    const wrapper = setup();
+    expect(mockGetSecretWord).toHaveBeenCalledTimes(1);
+  });
+
+  test('getSecretWord does not run on app update', () => {
+    const wrapper = setup();
+    mockGetSecretWord.mockClear();
+    wrapper.setProps({});
+    expect(mockGetSecretWord).toHaveBeenCalledTimes(0);
+  });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Congrats from './components/Congrats/Congrats';
 import GuessedWord from './components/GuessedWord/GuessedWord';
 import Input from './components/Input/Input';
 import { IGuessedWord } from './interfaces/guessedWord';
+import { getSecretWord } from './actions';
 
 function App() {
   const success = false;
   const secretWord = 'party';
   const guessedWords: IGuessedWord[] = [];
+
+  useEffect(() => {
+    getSecretWord();
+  }, []);
 
   return (
     <div className='container' data-test='component-app'>
