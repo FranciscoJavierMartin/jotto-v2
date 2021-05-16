58b82c7afd3d0b1ef8fd6539f343f3843f8889d0
Francisco <javiermartincuenca@gmail.com>
2021-05-16 19:32:35 +0200

    LanguagePicker tested

---
+++ src/components/LanguagePicker/LanguagePicker.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+interface LanguagePickerProps {
+  setLanguage: any;
+}
+
+const LanguagePicker: React.FC<LanguagePickerProps> = ({ setLanguage }) => {
+  const languages = [
+    { code: 'en', symbol: '🇬🇧' },
+    { code: 'emoji', symbol: '😊' },
+  ];
+
+  return (
+    <div data-test='component-language-picker'>
+      {languages.map((language) => (
+        <span
+          data-test='language-icon'
+          key={language.code}
+          onClick={() => setLanguage(language.code)}
+        >
+          {language.symbol}
+        </span>
+      ))}
+    </div>
+  );
+};
+
+export default LanguagePicker;
